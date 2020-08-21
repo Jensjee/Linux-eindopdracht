98278f927c714f86f9f33539fc58b5df4f338d29
DESKTOP-EQ5QRCV\jenss <jensslauerhoff@hotmail.com>
2020-08-21 16:11:17 +0200

    Installeer minion + host toevoegen

---
+++ voeghosttoe.sh
@@ -0,0 +1,96 @@
+#! /usr/bin/bash
+
+read -p "Naam client: " CLIENT_NAME #naam van de client
+read -p "IP: " IP_CLIENT #ip van de client
+
+cd "/usr/local/nagios/etc/objects/minions/"
+sudo cat > "$CLIENT_NAME.cfg" << EOF
+
+define host {
+        use                          linux-server
+        host_name                    $CLIENT_NAME
+        alias                        $CLIENT_NAME
+        address                      $IP_CLIENT
+        register                     1
+}
+
+define service {
+      host_name                       $CLIENT_NAME
+      service_description             PING
+      check_command                   check_ping!100.0,20%!500.0,60%
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       $CLIENT_NAME
+      service_description             Check Users
+      check_command           check_local_users!20!50
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       $CLIENT_NAME
+      service_description             Local Disk
+      check_command                   check_local_disk!20%!10%!/
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       $CLIENT_NAME
+      service_description             Check SSH
+      check_command                   check_ssh
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+
+define service {
+      host_name                       $CLIENT_NAME
+      service_description             Total Process
+      check_command                   check_local_procs!250!400!RSZDT
+      max_check_attempts              2
+      check_interval                  2
+      retry_interval                  2
+      check_period                    24x7
+      check_freshness                 1
+      contact_groups                  admins
+      notification_interval           2
+      notification_period             24x7
+      notifications_enabled           1
+      register                        1
+}
+EOF
